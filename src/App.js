5603401e3168c0f89bbc869b352ff39c1d0fdb04
Harsha <105192971+harshasai97@users.noreply.github.com>
2024-09-16 03:31:57 +0000

    Added to codesandbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,301 @@
-import "./styles.css";
+import mario from "./assets/mario.svg";
+import enemy from "./assets/enemy.svg";
+import "./App.css";
+import React from "react";
+import GameBoard from "./constants";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.element && (
+          <img
+            src={this.props.element}
+            style={{ display: this.props.display }}
+            alt=""
+          />
+        )}
+      </button>
+    );
+  }
 }
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.totalSquares = this.props.width * this.props.height;
+    this.state = {
+      squares: GameBoard,
+      marioLocation: Math.floor(
+        this.totalSquares / 2 - (this.props.width + this.props.height) / 4 - 1
+      ),
+      gameInitialized: false,
+      moves: 0,
+      yMoves: 0,
+      xMoves: 0,
+      lastMove: "+y",
+      gameOver: false,
+    };
+  }
+
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.checkForEnemies();
+    }, 100);
+  }
+
+  handleClick(i) {
+    if (i === this.state.marioLocation) this.checkForEnemies();
+  }
+
+  moveY(direction) {
+    if (
+      direction === "+" &&
+      this.state.marioLocation + 1 - this.props.height > 0
+    ) {
+      let newSquares = this.state.squares.slice();
+      newSquares[this.state.marioLocation] = {
+        element: "",
+        display: "none",
+      };
+      newSquares[this.state.marioLocation - this.props.height] = {
+        element: mario,
+        display: "block",
+      };
+
+      this.setState({
+        gameInitialized: true,
+        marioLocation: this.state.marioLocation - this.props.height,
+        squares: newSquares,
+        moves: this.state.moves + 1,
+        yMoves: this.state.yMoves + 1,
+        xMoves: 0,
+        lastMove: "+y",
+      });
+    } else if (
+      direction === "-" &&
+      this.state.marioLocation + this.props.height < this.totalSquares
+    ) {
+      let newSquares = this.state.squares.slice();
+      newSquares[this.state.marioLocation] = {
+        element: "",
+        display: "none",
+      };
+      newSquares[this.state.marioLocation + this.props.height] = {
+        element: mario,
+        display: "block",
+      };
+      this.setState({
+        gameInitialized: true,
+        marioLocation: this.state.marioLocation + this.props.height,
+        squares: newSquares,
+        moves: this.state.moves + 1,
+        yMoves: this.state.yMoves + 1,
+        xMoves: 0,
+        lastMove: "-y",
+      });
+    } else {
+      this.state.lastMove === "+y"
+        ? this.moveY("-")
+        : this.state.lastMove === "-y"
+        ? this.moveY("+")
+        : this.setState({ lastMove: "+y" });
+    }
+  }
+
+  moveX(direction) {
+    if (
+      direction === "+" &&
+      (this.state.marioLocation + 2) % this.props.width !== 1 &&
+      this.state.marioLocation + 1 < this.totalSquares
+    ) {
+      let newSquares = this.state.squares.slice();
+      newSquares[this.state.marioLocation] = {
+        element: "",
+        display: "none",
+      };
+      newSquares[this.state.marioLocation + 1] = {
+        element: mario,
+        display: "block",
+      };
+      this.setState({
+        gameInitialized: true,
+        marioLocation: this.state.marioLocation + 1,
+        squares: newSquares,
+        moves: this.state.moves + 1,
+        xMoves: this.state.xMoves + 1,
+        yMoves: 0,
+        lastMove: "+x",
+      });
+    } else if (
+      direction === "-" &&
+      this.state.marioLocation % this.props.width !== 0 &&
+      this.state.marioLocation - 1 >= 0
+    ) {
+      let newSquares = this.state.squares.slice();
+      newSquares[this.state.marioLocation] = {
+        element: "",
+        display: "none",
+      };
+      newSquares[this.state.marioLocation - 1] = {
+        element: mario,
+        display: "block",
+      };
+      this.setState({
+        gameInitialized: true,
+        marioLocation: this.state.marioLocation - 1,
+        squares: newSquares,
+        moves: this.state.moves + 1,
+        xMoves: this.state.xMoves + 1,
+        yMoves: 0,
+        lastMove: "-x",
+      });
+    } else {
+      this.state.lastMove === "+x"
+        ? this.moveX("-")
+        : this.state.lastMove === "-x"
+        ? this.moveX("+")
+        : this.setState({ lastMove: "+x" });
+    }
+  }
+
+  getMarioRange() {
+    let marioLoc = this.state.marioLocation;
+    let marioRange = [];
+    for (let i = 0; i < this.props.height; i++) {
+      if (
+        marioLoc >= i * this.props.width &&
+        marioLoc < i * this.props.width + this.props.width
+      ) {
+        marioRange = [
+          i * this.props.width,
+          i * this.props.width + this.props.width,
+        ];
+      }
+    }
+    return marioRange;
+  }
+
+  numberInRange(x, range) {
+    return x >= range[0] && x < range[1];
+  }
+
+  decideMove(enemyLocations) {
+    console.log("Enemy Locations: ", enemyLocations);
+    let distance = Math.abs(enemyLocations[0] - this.state.marioLocation);
+    let marioRange = this.getMarioRange();
+    if (
+      distance < this.props.width &&
+      enemyLocations[0] < this.state.marioLocation &&
+      this.numberInRange(enemyLocations[0], marioRange)
+    ) {
+      this.moveX("-");
+    } else if (
+      distance < this.props.width &&
+      enemyLocations[0] < this.state.marioLocation &&
+      !this.numberInRange(enemyLocations[0], marioRange)
+    ) {
+      this.moveY("+");
+    } else if (
+      distance < this.props.width &&
+      enemyLocations[0] > this.state.marioLocation &&
+      this.numberInRange(enemyLocations[0], marioRange)
+    ) {
+      this.moveX("+");
+    } else if (
+      distance < this.props.width &&
+      enemyLocations[0] > this.state.marioLocation &&
+      !this.numberInRange(enemyLocations[0], marioRange)
+    ) {
+      this.moveY("-");
+    } else if (
+      distance >= this.props.width &&
+      enemyLocations[0] < this.state.marioLocation
+    ) {
+      this.moveY("+");
+    } else {
+      this.moveY("-");
+    }
+  }
+
+  checkForEnemies() {
+    let enemies = this.state.squares.filter((square) => {
+      return square.element === enemy;
+    });
+    if (enemies.length === 0) {
+      this.setState({ gameOver: true });
+      // alert("Game over. Total moves to kill the enemies: " + this.state.moves);
+    } else {
+      this.decideMove(enemies.map((enemy) => enemy.value));
+    }
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        key={i}
+        value={i}
+        element={this.state.squares[i].element}
+        displayElement={this.state.squares[i].display}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  renderRows(squares) {
+    return <div className="board-row">{squares}</div>;
+  }
+
+  renderBoard() {
+    let board = [];
+    let rows = [];
+    for (let i = 0, squareNumber = 0; i < this.props.height; i++) {
+      for (let j = 0; j < this.props.width; j++) {
+        rows.push(this.renderSquare(squareNumber));
+        squareNumber++;
+      }
+      board.push(this.renderRows(rows));
+      rows = [];
+    }
+    return board;
+  }
+
+  render() {
+    return (
+      <div className="playGame">
+        {this.state.gameOver && (
+          <div className="game-completed">
+            <span>Game Completed!</span>
+            <button
+              className="restart-btn"
+              onClick={() => window.location.reload()}
+            >
+              Restart
+            </button>
+          </div>
+        )}
+        <div className="maze"> {this.renderBoard()}</div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    let width = 10;
+    let height = 10;
+    return (
+      <div className="game">
+        <div className="game-board">
+          <div className="title">MaZe GaMe</div>
+          <Board width={width} height={height} />
+          <h2 className="start-game-text">
+            Click on Mario to start the game!!
+          </h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
---
+++ src/constants.js
@@ -0,0 +1,504 @@
+const GameBoard = [
+  {
+    element: "/static/media/enemy.eaaa31a467200e45f7658fbaeec849b9.svg",
+    display: "block",
+    value: 0,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 1,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 2,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 3,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 4,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 5,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 6,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 7,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 8,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 9,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 10,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 11,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 12,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 13,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 14,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 15,
+  },
+  {
+    element: "/static/media/enemy.eaaa31a467200e45f7658fbaeec849b9.svg",
+    display: "block",
+    value: 16,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 17,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 18,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 19,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 20,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 21,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 22,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 23,
+  },
+  {
+    element: "/static/media/enemy.eaaa31a467200e45f7658fbaeec849b9.svg",
+    display: "block",
+    value: 24,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 25,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 26,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 27,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 28,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 29,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 30,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 31,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 32,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 33,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 34,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 35,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 36,
+  },
+  {
+    element: "/static/media/enemy.eaaa31a467200e45f7658fbaeec849b9.svg",
+    display: "block",
+    value: 37,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 38,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 39,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 40,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 41,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 42,
+  },
+  {
+    element: "/static/media/enemy.eaaa31a467200e45f7658fbaeec849b9.svg",
+    display: "block",
+    value: 43,
+  },
+  {
+    element: "/static/media/mario.9a038aacfabb2af3991b6d2a4661ee4c.svg",
+    display: "block",
+    value: 44,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 45,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 46,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 47,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 48,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 49,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 50,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 51,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 52,
+  },
+  {
+    element: "/static/media/enemy.eaaa31a467200e45f7658fbaeec849b9.svg",
+    display: "block",
+    value: 53,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 54,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 55,
+  },
+  {
+    element: "/static/media/enemy.eaaa31a467200e45f7658fbaeec849b9.svg",
+    display: "block",
+    value: 56,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 57,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 58,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 59,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 60,
+  },
+  {
+    element: "/static/media/enemy.eaaa31a467200e45f7658fbaeec849b9.svg",
+    display: "block",
+    value: 61,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 62,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 63,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 64,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 65,
+  },
+  {
+    element: "/static/media/enemy.eaaa31a467200e45f7658fbaeec849b9.svg",
+    display: "block",
+    value: 66,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 67,
+  },
+  {
+    element: "/static/media/enemy.eaaa31a467200e45f7658fbaeec849b9.svg",
+    display: "block",
+    value: 68,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 69,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 70,
+  },
+  {
+    element: "/static/media/enemy.eaaa31a467200e45f7658fbaeec849b9.svg",
+    display: "block",
+    value: 71,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 72,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 73,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 74,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 75,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 76,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 77,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 78,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 79,
+  },
+  {
+    element: "/static/media/enemy.eaaa31a467200e45f7658fbaeec849b9.svg",
+    display: "block",
+    value: 80,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 81,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 82,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 83,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 84,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 85,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 86,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 87,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 88,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 89,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 90,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 91,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 92,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 93,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 94,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 95,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 96,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 97,
+  },
+  {
+    element: "",
+    display: "none",
+    value: 98,
+  },
+  {
+    element: "/static/media/enemy.eaaa31a467200e45f7658fbaeec849b9.svg",
+    display: "block",
+    value: 99,
+  },
+];
+
+export default GameBoard;
